cabd696b80f77d985544faf0ecf3c7e7f2145a35
Your Actual Name <Your Actual Email>
2014-05-07 14:35:35 -0700

    add devise aesthetics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,9 @@ Fitjar::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
+  
   config.log_formatter = ::Logger::Formatter.new
+
+  #CHANGE TO FITJAR.CO AFTER
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Fitjar::Application.routes.draw do
+  devise_for :users
   resources :challenges
 
   get "pages/about"

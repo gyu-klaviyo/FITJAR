cd805510ad40241ddeeb26bd4928bcbe9fe0e031
Your Actual Name <Your Actual Email>
2014-05-08 16:52:18 -0700

    issue with heroku gem

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,9 +1,9 @@
 class Challenge < ActiveRecord::Base
 	  
   if Rails.env.development?
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "Logo-Jar.jpg"
   else
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "Logo-Jar.jpg",
         :storage => :dropbox,
         :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
         :path => ":style/:id_:filename"
